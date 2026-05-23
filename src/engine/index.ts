896358f1731ac4af1e7e222b3b6364a958bbb470
KNOCKS <199303375+KNOCKSSTUDiOS@users.noreply.github.com>
2026-05-22 19:04:53 -0700

    Create index.ts

---
+++ src/engine/index.ts
@@ -0,0 +1,9 @@
+import { Q44Core } from "./q44-core";
+import { StudioAssets } from "./studioAssets";
+
+export const SapphireQ44 = {
+  engine: Q44Core,
+  assets: StudioAssets,
+  studio: "HOLLYWOOD IMAGING STUDIO",
+};
+
